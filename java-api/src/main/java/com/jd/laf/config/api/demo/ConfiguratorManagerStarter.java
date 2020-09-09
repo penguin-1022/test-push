8e7e6f260d548e55fa7dafa9b5e52d1501437e58
andre <bjliuyong@jd.com>
2020-09-09 09:55:23 +0800

    resource name

--- java-api/src/main/java/com/jd/laf/config/api/demo/ConfiguratorManagerStarter.java
+++ java-api/src/main/java/com/jd/laf/config/api/demo/ConfiguratorManagerStarter.java
@@ -4,6 +4,8 @@ import com.jd.laf.config.Configuration;
 import com.jd.laf.config.ConfiguratorManager;
 import com.jd.laf.config.Property;
 import com.jd.laf.config.Resource;
+import com.jd.laf.config.listener.ConfigurationListener;
+import com.jd.laf.config.listener.PropertyListener;
 
 /**
  * Created by bjliuyong on 2018/12/18.
@@ -18,10 +20,17 @@ public class ConfiguratorManagerStarter {
         ConfiguratorManager configuratorManager = ConfiguratorManager.getInstance() ;
         //设置appName，jone或者jdos部署可自动获取，无需配置
         configuratorManager.setApplication(appName);
-        //创建资源对象，此处直接使用ducc远程
-        Resource resource = new Resource("ucc", uri);
+
+        String resourceName = "myResourceName";
+
+        //创建资源对象，此处直接使用ducc远程，Name属性很重要，下面会用到
+        Resource resource = new Resource(resourceName, uri);
         //给配置管理器添加管理的资源
         configuratorManager.addResource(resource);
+
+        //可以监听多个Resource
+        //configuratorManager.addResource(resource); 调用多次
+
         //启动之后才可以获取配置
         configuratorManager.start();
 
@@ -30,13 +39,33 @@ public class ConfiguratorManagerStarter {
         System.out.println("jdbc.url:" + property1.getString());
 
         //获取配置 方式2 (获取指定配置源下指定配置)
-        Property property2 = configuratorManager.getProperty("ucc", "jdbc.url");
+        Property property2 = configuratorManager.getProperty(resourceName, "jdbc.url");
         System.out.println("jdbc.url:" + property2.getString());
 
         //获取配置 方式3 (获取指定配置源下配置集合)
-        Configuration configuraiton = configuratorManager.getConfiguration("ucc");
+        Configuration configuraiton = configuratorManager.getConfiguration(resourceName);
         System.out.println("ucc configuration:" + configuraiton);
         System.out.println("jdbc.url:" + configuraiton.getProperty("jdbc.url").getString());
+
+        //添加监听器，配置项维度的监听器
+        configuratorManager.addListener(new PropertyListener.CustomPropertyListener("test.key") {
+            @Override
+            public void onUpdate(Property property) {
+                System.out.println(property);
+            }
+        });
+
+        //添加监听器，resource维度的监听器
+        configuratorManager.addListener(new ConfigurationListener.CustomConfigurationListener(resourceName) {
+            @Override
+            public void onUpdate(Configuration configuration) {
+                System.out.println(configuration);
+            }
+        });
+
+        synchronized (configuraiton){
+            configuraiton.wait();
+        }
 
         //java进程退出时，可进行关闭
         configuratorManager.stop();
