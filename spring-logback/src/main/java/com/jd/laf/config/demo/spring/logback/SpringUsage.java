5ef01a43cfdc54f40214e6f42d36105f903c9419
lixiaowei1 <100086100010A>
2021-11-03 17:55:01 +0800

    comment

--- spring-logback/src/main/java/com/jd/laf/config/demo/spring/logback/SpringUsage.java
+++ spring-logback/src/main/java/com/jd/laf/config/demo/spring/logback/SpringUsage.java
@@ -6,6 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringUsage {
 
+    // wiki地址：https://git.jd.com/laf/laf-config/wikis/动态日志配置
+
     public static void main(String args[]) throws Exception {
         ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("/spring.xml");
         Config config = appContext.getBean(Config.class) ;
