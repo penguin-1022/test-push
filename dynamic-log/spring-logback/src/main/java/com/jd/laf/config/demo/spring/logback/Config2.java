17cc16ef2bd326384b29d931925e9596746d6623
yangzhiwei <yangzhiwei@jd.com>
2022-06-16 22:02:11 +0800

    补充动态日志demo，如： log4j demo；优化 logback demo 代码

---
+++ dynamic-log/spring-logback/src/main/java/com/jd/laf/config/demo/spring/logback/Config2.java
@@ -0,0 +1,21 @@
+package com.jd.laf.config.demo.spring.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config2 {
+    private static final Logger logger = LoggerFactory.getLogger(Config2.class);
+
+    public Config2() {
+    }
+
+    public void loggerLevel() {
+        logger.debug("1 debug level.");
+        logger.info("2 info level.");
+        logger.warn("3 warning level.");
+        logger.error("4 error level.");
+        System.out.println("************************************************************");
+    }
+}
---
+++ dynamic-log/spring-logback/src/main/java/com/jd/laf/config/demo/spring/logback/LogbackDemoLauncher.java
@@ -0,0 +1,22 @@
+package com.jd.laf.config.demo.spring.logback;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LogbackDemoLauncher {
+
+    public static void main(String args[]) throws Exception {
+        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("/spring.xml");
+        Config1 config1 = appContext.getBean(Config1.class);
+        Config2 config2 = appContext.getBean(Config2.class);
+        Config3 config3 = appContext.getBean(Config3.class);
+        Config4 config4 = appContext.getBean(Config4.class);
+
+        while (true) {
+            config1.loggerLevel();
+            config2.loggerLevel();
+            config3.loggerLevel();
+            config4.loggerLevel();
+            Thread.sleep(1000);
+        }
+    }
+}
