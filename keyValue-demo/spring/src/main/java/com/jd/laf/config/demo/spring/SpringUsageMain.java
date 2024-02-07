6f4c854d00a9bdb50e0a4c02a63160a3e8d8d452
yangzhiwei8 <yangzhiwei@jd.com>
2024-02-07 12:55:09 +0800

    up

--- keyValue-demo/spring/src/main/java/com/jd/laf/config/demo/spring/SpringUsageMain.java
+++ keyValue-demo/spring/src/main/java/com/jd/laf/config/demo/spring/SpringUsageMain.java
@@ -1,28 +1,31 @@
 package com.jd.laf.config.demo.spring;
 
-import com.jd.laf.config.ConfiguratorManager;
 import com.jd.laf.config.demo.common.beans.DuccBean;
 import com.jd.laf.config.demo.common.beans.SupportConvertTypeSimple;
 import com.jd.laf.config.demo.common.bo.User;
-import com.jd.laf.config.demo.spring.jsf.HelloService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Created by bjliuyong on 2018/11/26.
- */
 public class SpringUsageMain {
 
     private static Logger LOG = LoggerFactory.getLogger(SpringUsageMain.class);
 
     public static void main(String args[]) throws Exception {
+        //初始化 spring 容器
         ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("/spring.xml");
-        HelloService helloService = (HelloService) appContext.getBean("helloService");
 
+        /**
+         * 容器中的 bean，查看这个 bean 的源码可以发现 DUCC SDK 支持的常用注解和使用方式。
+         * DUCC 推荐使用的注解： @LafValue
+         * 注意： 尽管 DUCC SDK 兼容了 spring 的 @Value 注解，但是这对底层框架是有侵入的，不建议使用 spring 注解监听 DUCC 配置。
+         */
         DuccBean duccBean = appContext.getBean(DuccBean.class) ;
+
+        /**
+         *
+         */
         SupportConvertTypeSimple simple = appContext.getBean(SupportConvertTypeSimple.class);
-        ConfiguratorManager manager = appContext.getBean(ConfiguratorManager.class);
 
         User user = appContext.getBean(User.class);
         Thread.sleep(2000);
@@ -31,12 +34,6 @@ public class SpringUsageMain {
             LOG.info("{}", duccBean);
             LOG.info("{}", user);
             LOG.info("{}", simple);
-
-            LOG.error("---------------------------------------------------------------------------------------");
-            LOG.debug("======================debug===================================");
-            LOG.info("=======================info==================================");
-            LOG.warn("=======================warn==================================");
-            LOG.error("=======================error==================================");
 
             Thread.sleep(2000);
         }
