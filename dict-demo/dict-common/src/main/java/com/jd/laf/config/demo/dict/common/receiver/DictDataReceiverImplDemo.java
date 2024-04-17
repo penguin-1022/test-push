73e1bac0d183428be8f8a34d273deca63487305f
ronggangping <ronggangping@jd.com>
2024-04-17 20:47:30 +0800

    code: 词表demo修正

--- dict-demo/dict-common/src/main/java/com/jd/laf/config/demo/dict/common/receiver/DictDataReceiverImplDemo.java
+++ dict-demo/dict-common/src/main/java/com/jd/laf/config/demo/dict/common/receiver/DictDataReceiverImplDemo.java
@@ -9,7 +9,9 @@ import com.jd.laf.config.dict.datum.OperateTypeEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Title: todo <br>
@@ -24,22 +26,21 @@ import java.util.*;
  */
 public class DictDataReceiverImplDemo implements DictDataReceiver {
     private static Logger LOGGER = LoggerFactory.getLogger(DictDataReceiverImplDemo.class);
-    private Map<Long, Collection<Property>> data = new HashMap<>();
-    private List<Collection<Property>> properties = new ArrayList<>();
+    private Map<Long, Collection<Property>> dataForUse = new HashMap<>();
+    private Map<Long, Collection<Property>> dataForProcess = new HashMap<>();
     //是否是增量数据，对应的resource 打开了增量模式：useIncrementModel=true
     private boolean incrementData;
+    private boolean error;
     private DictMeta.Field idField = new DictMeta.Field("id");
 
     @Override
     public void begin(DictTable dictTable) {
-        boolean incrementData = dictTable.isIncrementData();
-        if (incrementData) {//打开了增量模式才需要判断，
-            this.incrementData = incrementData;
-            LOGGER.info("table:{}，begin increment data", dictTable.getTableName());
-        } else {
-            LOGGER.info("table:{}，begin all data", dictTable.getTableName());
-            data.clear();
-            properties.clear();
+        this.incrementData = dictTable.isIncrementData();
+        LOGGER.info("table:{}，incrementData:{}", dictTable.getTableName(), incrementData);
+        if (!incrementData) {
+            //打开增量模式后才需要判断
+            //推送全量数据，清除旧数据
+            dataForProcess.clear();
         }
     }
 
@@ -47,12 +48,8 @@ public class DictDataReceiverImplDemo implements DictDataReceiver {
     public void onLine(DictLine dictLine) {
         Map<DictMeta.Field, Property> line = dictLine.getLine();
         Property property = line.get(idField);
-        if (property == null) {
-            //老版本的id值，由页面控制---是否同步网盘id
-            //只要发布过一次后，都会带有id值
-            properties.add(line.values());
-            return;
-        }
+        //老版本的id值，由页面控制---是否同步网盘id
+        //只要发布过一次后，都会带有id值
         Long id = property.getLong();
         if (incrementData) {
             OperateTypeEnum operateTypeEnum = dictLine.getOperateTypeEnum();
@@ -60,24 +57,30 @@ public class DictDataReceiverImplDemo implements DictDataReceiver {
             switch (operateTypeEnum) {
                 case add:
                 case update:
-                    this.data.put(id, line.values());
+                    this.dataForProcess.put(id, line.values());
                     break;
                 case delete:
-                    this.data.remove(id);
+                    this.dataForProcess.remove(id);
                     break;
             }
         } else {
-            this.data.put(id, line.values());
+            this.dataForProcess.put(id, line.values());
         }
     }
 
     @Override
     public void end(DictTable dictTable) {
-        LOGGER.info("finish table has data size:{}  old datasize:{}", this.data.size(), this.properties.size());
+        LOGGER.info("finish table error:{} has data size:{}  old datasize:{}", error, this.dataForProcess.size(), this.dataForUse.size());
+        if (!error) {
+            //处理成功后使用新版本数据
+            Map<Long, Collection<Property>> dataForUseTemp = new HashMap<>();
+            dataForUseTemp.putAll(dataForProcess);
+            dataForUse = dataForUseTemp;
+        }
     }
 
     @Override
     public void fail(DictTable dictTable) {
-
+        error = true;
     }
 }
